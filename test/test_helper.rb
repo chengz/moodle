265ecde6f3edaa72e41de3e47cb2698e363315d1
Cheng Zhu <cheng@weteachme.com>
2014-12-03 00:50:34 +1100

    add coverall

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+require 'coveralls'
+Coveralls.wear!
 require "minitest/matchers"
 require 'minitest/autorun'
 require 'mocha/setup'
